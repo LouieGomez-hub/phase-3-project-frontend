d091767365fc6674b9d0335516e226d8e3235616
louie_gomez <kinggomez027.lg@gmail.com>
2022-12-14 18:08:57 -0500

    created a form to add new superheroes

--- src/components/NewSupeForm.js
+++ src/components/NewSupeForm.js
@@ -1,11 +1,53 @@
 import React, { useState, useEffect } from 'react';
 
-function NewSupeForm() {
+function NewSupeForm({onAddSuperhero}) {
+    const [name, setName] = useState("");
+    const [power, setPower] = useState("");
+    
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        setName("");
+        setPower("");
+
+        const supeData = {
+            name: name,
+            power: power
+        }
+
+        fetch("http://localhost:9292/superheros", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(supeData)
+        })
+        .then((res) => res.json())
+        .then((newSupe) => onAddSuperhero(newSupe));
+    }
+
     return (
-        <div></div>
+        <div>
+            <form className='NewSupe' onSubmit={handleSubmit}>
+                <label>Add Superhero:</label>
+                <input
+                    type="text"
+                    name="name"
+                    value={name}
+                    placeholder="Name..."
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <input
+                    type="text"
+                    name="name"
+                    value={power}
+                    placeholder="Power..."
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <input className='SubmitInput' type="submit" value="Submit" />
+            </form>
+        </div>
     )
 }
-
-
 
 export default NewSupeForm;
