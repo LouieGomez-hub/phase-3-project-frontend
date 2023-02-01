57e452f60b580e0ef4ad231d960156383d6c47ab
louie_gomez <kinggomez027.lg@gmail.com>
2023-02-01 12:59:36 -0500

    added text for about page

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 function About() {
     return (
-        <div></div>
+        <div>
+            <h1>About Made Super</h1>
+            <p>What is Made Super? The simple answer is this a web page full of fake generated superheroes. However, what this web application really is, is a starting point for creation. For anyone interested in superheroes, powers/abilities or anyone who just has an idea that needs a way to be expressed, this is what Made Super is all about. This app gives you the freedom to create a powered being that is your very own, assign a name and power(s) to your character and you've now layed the groundwork for something you can do anything with. Whether you're just having a bit of creative fun or giving life to an old idea you've been waiting to get to, Made Super is Made For You By You.</p>
+        </div>
     )
 }
 
