95706ce5518786fca7b1f62de1ba95e3e43f4d3c
louie_gomez <kinggomez027.lg@gmail.com>
2023-02-02 20:48:51 -0500

    delete function working

--- src/components/Superheros.js
+++ src/components/Superheros.js
@@ -2,18 +2,28 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import NewSupeForm from './NewSupeForm';
 
-function Superheros({ superheros }) {
+function Superheros({ superheros, onDeleteClick }) {
     let heroSuite;
 
     heroSuite = superheros.map(superhero => {
         let { id, name, power } = superhero
+
+        function handleDeleteClick() {
+          fetch(`http://localhost:9292/superheros/${superhero.id}`, {
+            method: "DELETE",
+          })
+          .then((res) => res.json())
+          .then((deletedSuperhero) => onDeleteClick(deletedSuperhero));
+          
+        }
 
         return (
           <div key={id} className='suite-container'>
             <div className='cards'>
               <h1 className='content-name'>Alias: <em>{name}</em></h1>
               <p className='content-attr'><strong>Powers:</strong> <em>{power}</em></p>
-              <Link className='btn' to={`/superheros/${superhero.id}`}>View Comments</Link>  
+              <Link className='btn' to={`/superheros/${superhero.id}`}>View Comments</Link>
+              <button onClick={handleDeleteClick}>X</button>  
             </div>
           </div>
         )
