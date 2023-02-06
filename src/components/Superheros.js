46be9e110da270f3b2a9a6a44b52e05e92cf9bcb
louie_gomez <kinggomez027.lg@gmail.com>
2023-02-06 16:02:07 -0500

    undo

--- src/components/Superheros.js
+++ src/components/Superheros.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React } from 'react';
 import { Link } from 'react-router-dom';
 import NewSupeForm from './NewSupeForm';
 
@@ -17,22 +17,6 @@ function Superheros({ superheros, onDeleteClick, onUpdateSuperhero }) {
           
         }
 
-          function handleEditSubmit(e) {
-            e.preventDefault();
-            fetch(`http://localhost:9292/superheros/${superhero.id}`, {
-              method: "PATCH",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({
-                name: name,
-                power: power,
-              }),
-            })
-            .then((res) => res.json())
-            .then((updatedSuperhero) => onUpdateSuperhero(updatedSuperhero));
-          }
-
         return (
           <div key={id} className='suite-container'>
             <div className='cards'>
@@ -40,11 +24,10 @@ function Superheros({ superheros, onDeleteClick, onUpdateSuperhero }) {
               <p className='content-attr'><strong>Powers:</strong> <em>{power}</em></p>
               <Link className='btn' to={`/superheros/${superhero.id}`}>View Comments</Link>
               <button className='deletebtn' onClick={handleDeleteClick}>Delete</button>
-              <button className='deletebtn' onClick={handleEditSubmit}>Edit</button>
             </div>
           </div>
         )
-    })
+      })
 
     return (
         <div>
